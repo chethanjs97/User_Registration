ed5cddfc14c692651158f57cffa47ab0ff4a83b4
chethanjs97 <chethanjschethu@gmail.com>
2020-10-15 08:21:39 +0530

    Added : First Name Starts with Capital letter

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,11 @@
-#!/bin/bash -x
-echo "Welcome to User registration "
+#!/bin/bash
+shopt -s extglob
+echo "Welcome to User Registration"
+read -p "Enter the First Name" Fname
+pat="^[A-Z]{1}[a-z]{2,}$"
+if [[ $Fname =~ $pat ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
